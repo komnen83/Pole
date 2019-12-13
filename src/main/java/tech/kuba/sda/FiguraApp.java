aeded62af3cda12b93b0dd33cacc445f1996e3f0
komnen83 <kuba.bauman@gmail.com>
2019-12-13 23:59:39 +0100

    pole

--- src/main/java/tech/kuba/sda/FiguraApp.java
+++ src/main/java/tech/kuba/sda/FiguraApp.java
@@ -6,37 +6,40 @@ public class FiguraApp {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-//        System.out.print("Podaj ilosc figur: ");
-//        int numbersOfFigure = scanner.nextInt();
 
-        System.out.print("Podaj figure: ");
-        String figura = scanner.nextLine();
+        System.out.print("Wprowadz ilosc figur: ");
+        int licznik = scanner.nextInt();
 
-        switch (figura) {
-            case "kolo":
+        while (licznik >= 0){
+            System.out.print("Podaj figure: ");
+            String figura = scanner.nextLine();
+            switch (figura) {
+                case "kolo":
 
-                System.out.print("Podaj promien: ");
-                int promien = scanner.nextInt();
-                Kolo kolo = new Kolo(promien);
-                scanner.nextLine();
-                System.out.println("Pole kola wynosi: " + kolo.obliczPowierzchnie() + "\n");
-                break;
+                    System.out.print("Podaj promien: ");
+                    int promien = scanner.nextInt();
+                    Kolo kolo = new Kolo(promien);
+                    scanner.nextLine();
+                    System.out.println("Pole kola wynosi: " + kolo.obliczPowierzchnie() + "\n");
+                    break;
 
-            case "prostokat":
-                System.out.print("Podaj długość boku a: ");
-                int a = scanner.nextInt();
-                System.out.print("Podaj długość boku b: ");
-                int b = scanner.nextInt();
-                Prostokat prostokat = new Prostokat(a, b);
-                System.out.println("Pole prostokata wynosi: " + prostokat.obliczPowierzchnie() + "\n");
-                break;
+                case "prostokat":
+                    System.out.print("Podaj długość boku a: ");
+                    int a = scanner.nextInt();
+                    System.out.print("Podaj długość boku b: ");
+                    int b = scanner.nextInt();
+                    Prostokat prostokat = new Prostokat(a, b);
+                    System.out.println("Pole prostokata wynosi: " + prostokat.obliczPowierzchnie() + "\n");
+                    break;
 
-            case "kwadrat":
-                System.out.print("Podaj długość boku a: ");
-                int c = scanner.nextInt();
-                Kwadrat kwadrat = new Kwadrat(c);
-                System.out.println("Pole kwadratu wynosi: " + kwadrat.obliczPowierzchnie() + "\n");
-                break;
+                case "kwadrat":
+                    System.out.print("Podaj długość boku a: ");
+                    int c = scanner.nextInt();
+                    Kwadrat kwadrat = new Kwadrat(c);
+                    System.out.println("Pole kwadratu wynosi: " + kwadrat.obliczPowierzchnie() + "\n");
+                    break;
+            }
+            licznik--;
         }
     }
 }
